bee2e94dd59e9ac79bfc1d7fdb46f610127e1512
emilyalvae <emilisa23@gmail.com>
2023-05-28 21:45:56 -0500

    jwt movie

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,10 @@
 from .models import Movie
 from .serializer import MovieSerializers
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializers
+    permission_classes = [IsAuthenticated]
     
